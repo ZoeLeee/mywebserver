a45efd79169addba566cd39ae2539b85b9d57a1d
Zoe <33168975+ZoeLeee@users.noreply.github.com>
2018-10-24 19:38:10 +0800

    调试

--- Routes/route.js
+++ Routes/route.js
@@ -1,34 +1,39 @@
 const express = require('express');
-const path=require('path');
-const fs=require('fs');
+const path = require('path');
+const fs = require('fs');
 const router = express.Router();
-const storage=require('../Utility/utility').storage;
-const createFolder=require('../Utility/utility').createFolder;
-var multer  = require('multer');
+const storage = require('../Utility/utility').storage;
+const createFolder = require('../Utility/utility').createFolder;
+var multer = require('multer');
 
 
 // var uploadFolder = './upload/';
 // createFolder(uploadFolder);
 // 创建 multer 对象
-var upload = multer({ storage:storage });
+var upload = multer({ storage: storage });
 
 //上传
 router.post('/upload', function (req, res, next) {
-  let filePath=path.resolve(__dirname,'../../');
-  fs.readdir(filePath,(err,files)=>{
-    if(!err){
-      files.forEach(f =>
-        {
-          let tmpPath=path.join(filePath,f)
-            if (f.indexOf("bundle.js") !== -1)
-                fs.unlink(tmpPath, e =>
-                {
-                });
-        })
+  let filePath = path.resolve(__dirname, '../../');
+  console.log('filePath: ', filePath);
+  fs.readdir(filePath, (err, files) => {
+    if (!err) {
+      files.forEach(f => {
+        let tmpPath = path.join(filePath, f)
+        if (f.indexOf("bundle.js") !== -1)
+          fs.unlink(tmpPath, e => {
+          });
+      })
+    }
+    else {
+      res.send({ success: "no" })
     }
   })
-  upload.any()(req,res,err=>{
-    res.send({success:"ok"})
+  upload.any()(req, res, err => {
+    if (err)
+      res.send({ success: "no" });
+    else
+      res.send({ success: "ok" });
   })
 });
 
--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-app.listen(3000,"www.dodream.online",()=>{
+app.listen(3000,()=>{
   console.log('listening on port 3000!');
 })
 
