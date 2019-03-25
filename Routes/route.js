09feb8e459a8c32b17c836c0b26773cf5a9f7e20
Zoe <lizhou1021@qq.com>
2019-03-25 21:59:01 +0800

    保持登陆状态，引入session

--- Routes/route.js
+++ Routes/route.js
@@ -6,19 +6,25 @@ const storage = require('../Utility/utility').storage;
 const ObjectID = require('mongodb').ObjectID;
 // const createFolder = require('../Utility/utility').createFolder;
 var multer = require('multer');
-const db=require('../Utility/utility').connectDB;
+const db = require('../Utility/utility').connectDB;
 // var uploadFolder = './upload/';
 // createFolder(uploadFolder);
 // 创建 multer 对象
 var upload = multer({ storage: storage });
 
-const ArticleModel=require("../Db/Mongodb").Article;
-const UserModel=require('../Db/Mongodb').User;
+const ArticleModel = require("../Db/Mongodb").Article;
+const UserModel = require('../Db/Mongodb').User;
+
+const REQUEST_CODE = {
+  Ok: 0,
+  Err: -1,
+  Other: 1,
+}
 
 //上传
 router.post('/upload', function (req, res, next) {
   let filePath = path.resolve(__dirname, '../');
-  let files=fs.readdirSync(filePath);
+  let files = fs.readdirSync(filePath);
   files.forEach(f => {
     let tmpPath = path.join(filePath, f);
     if (f.includes("bundle.js"))
@@ -26,112 +32,156 @@ router.post('/upload', function (req, res, next) {
       });
   })
   upload.any()(req, res, err => {
-    if (err){
-      console.log(err);
-      res.send({ success: "no",data:err});
+    if (err) {
+      res.send({ code: REQUEST_CODE.Err, msg: err });
     }
-    else{
-      console.log("上传成功，上传位置在"+filePath+"文件数："+req.files.length)
-      res.send({ success: "ok" });
+    else {
+      console.log("上传成功，上传位置在" + filePath + "文件数：" + req.files.length)
+      res.send({ code: REQUEST_CODE.Ok });
     }
   })
 });
 
 //获取文章列表
 router.get('/articles', function (req, res, next) {
-  ArticleModel.find(function(err,articles){
+  ArticleModel.find(function (err, articles) {
     if (err) {
       res.send({
-        success:"none",
-        data:err
+        code: REQUEST_CODE.Err,
+        data: err
       })
     };
     res.send({
-      success:"ok",
-      data:articles
+      code: REQUEST_CODE.Ok,
+      data: articles
     })
   })
 });
 //获取文章
 router.get('/article/:articleId', function (req, res, next) {
   let articleId = req.params.articleId;
-  ArticleModel.find({_id:articleId},function(err,article){
+  ArticleModel.find({ _id: articleId }, function (err, article) {
     if (err) {
       res.send({
-        success:"no",
-        data:err
+        code: REQUEST_CODE.Err,
+        data: err
       })
     };
     res.send({
-      success:"ok",
-      data:article
+      code: REQUEST_CODE.Ok,
+      data: article
     })
   })
 });
 //写文章
 router.post('/write', function (req, res, next) {
   var article = new ArticleModel(req.body);
-  article.save(function(err,article){
+  article.save(function (err, article) {
     if (err) {
       res.send({
-        success:"no",
+        code: REQUEST_CODE.Err,
       })
     };
     res.send({
-      success:"ok",
-      id:article._id
+      code: REQUEST_CODE.Ok,
+      data: article._id
     })
   })
 });
 //更新文章信息
 router.post('/update', function (req, res, next) {
-  let id=req.body._id;
-  ArticleModel.updateOne({_id:id},req.body,function(err,r){
+  let id = req.body._id;
+  ArticleModel.updateOne({ _id: id }, req.body, function (err, r) {
     if (err) {
       res.send({
-        success:"no",
+        code: REQUEST_CODE.Err,
       })
     };
     res.send({
-      success:"ok",
+      code: REQUEST_CODE.Ok,
     })
   })
 });
 
 router.post('/register', function (req, res, next) {
   var user = new UserModel(req.body);
-  user.save(function(err,u){
+  user.save(function (err, u) {
     if (err) {
       res.send({
-        success:"no",
+        code: REQUEST_CODE.Err,
+        msg: "注册失败"
       })
     };
     res.send({
-      success:"ok",
-      id:u._id
+      code: REQUEST_CODE.Ok,
+      data: u._id,
+      msg: "注册成功"
     })
   })
 });
+
+
+/*
+    用户登录的接口
+        提交的验证：
+            1.用户名不能为空
+            2.密码不能为空
+        数据库的验证：
+            1.用户名是否存在
+            2.若存在密码是否正确
+*/
+
 router.post('/login', function (req, res, next) {
   let data = req.body;
-  console.log('data: ', data);
-  UserModel.find({uname:data.uname,pwd:data.pwd},function(err,u){
+  UserModel.find({ uname: data.uname, pwd: data.pwd }, function (err, users) {
     if (err) {
       res.send({
-        success:"no",
-        data:err
+        code: REQUEST_CODE.Err,
+        data: err
       })
     };
-    if(u.length>0){
+    if (users.length > 0) {
+      //登录成功
+      req.session.user = users[0];
+      req.session.isLogin = true;
+      res.cookie("session_id", "123456");
+      let data = {
+        userInfo: users[0],
+      };
       res.send({
-        success:"ok",
-        data:u[0]
+        code: REQUEST_CODE.Ok,
+        msg: "登陆成功",
+        data
       })
     }
     else
-    res.send({
-      success:"no"
-    })
+      res.send({
+        code: REQUEST_CODE.Other,
+        msg: "用户名或者密码错误"
+      })
   })
 });
+router.post('/loginout', function (req, res, next) {
+  //清除session，cookie
+  req.session.destroy(function () {
+    res.clearCookie("user", {});
+    res.cookie("isLogin", "false");
+    res.redirect("/");
+  });
+  res.send({
+    code: REQUEST_CODE.Ok,
+    msg: "退出成功"
+  })
+})
+router.post('/loginstatus', function (req, res, next) {
+  if (req.session.user)
+    res.send({
+      code: REQUEST_CODE.Ok,
+    })
+  else
+    res.send({
+      code: REQUEST_CODE.Err,
+      msg:"未登陆"
+    })
+})
 module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var path = require('path');
-
+const Cookies = require("cookies");
 var indexRouter = require('./Routes/route');
+const session=require('express-session');
 
 var app = express();
 app.all('*',(req,res,next)=>{
-  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Origin","http://localhost:8080");
   res.header('Access-Control-Allow-Methods','PUT,GET,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header('Access-Control-Allow-Headers','Content-Type');
@@ -13,11 +14,17 @@ app.all('*',(req,res,next)=>{
   next();
 })
 
+app.use(session({
+  secret: "zoe",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {user:"zoe",maxAge: 14*24*60*60*1000}
+}));
 
 //输出服务器记录
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.use('/', indexRouter);
 
