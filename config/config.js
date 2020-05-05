ebaf0d9f78e41d72a1d5aca7721fd7d2e11a8b64
Zoe <lizhou1021@qq.com>
2020-05-05 14:14:57 +0800

    修改本地数据库地址

--- config/config.js
+++ config/config.js
@@ -1,8 +1,11 @@
 const UNAME="zoelee";
 const PWD="lz901021";
 
+const TX_NAME="lizhou1021";
+const TX_PWD="lz901021";
+
 exports.defaultConfig={
   dbUrl:`mongodb://${UNAME}:${PWD}@ds117681.mlab.com:17681/learnforzoe`, 
-  aLiDbUrl:"mongodb://127.0.0.1:27017/myWeb",
+  aLiDbUrl:`mongodb://${TX_NAME}:${TX_PWD}@122.51.246.156:27017/myweb`, 
   staticUrl:"/usr/share/nginx/html/"
 }
