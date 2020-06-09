f72276b9fd5ab018e185bb870d1bd709d68b0fed
Zoe <lizhou1021@qq.com>
2020-06-09 22:55:57 +0800

    优化

--- src/utils/hosts.ts
+++ src/utils/hosts.ts
@@ -1,12 +1,12 @@
 export const ReqApi={
-  Login:"/api/login",
-  Register:"/api/register",
-  LoginOut:"/api/loginout",
+  Login:"/login",
+  Register:"/register",
+  LoginOut:"/loginout",
   LoginStatus:"/loginstatus",
-  Update:"/api/update",
-  Write:"/api/write",
-  Article:"/api/article",
-  Upload:"/api/upload",
-  Articles:"/api/articles",
+  Update:"/update",
+  Write:"/write",
+  Article:"/article",
+  Upload:"/upload",
+  Articles:"/articles",
 }
 
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const RequestServer = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "http://localhost:3000/api"
 });
 
 export const StoreageKeys={
